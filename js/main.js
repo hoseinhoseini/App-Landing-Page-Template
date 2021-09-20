4c141a412015ce4195a356b97c8ad30ceba50de7
HosseinHosseini <hoseinhoseini2171384@gmail.com>
2021-09-20 15:04:12 +0430

    add testimonials carousel

--- js/main.js
+++ js/main.js
@@ -86,3 +86,27 @@ $('.screenshots-carousel').owlCarousel({
         },
     },
 })
+
+// ----------------------------------------------------------------
+// --------------------- Testimonials Carousel -----------------
+// ----------------------------------------------------------------
+$('.testimonials-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+
+        600: {
+            items: 2,
+        },
+
+        1000: {
+            items: 3,
+        },
+    },
+})
