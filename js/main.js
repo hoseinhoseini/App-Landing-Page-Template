220497fea9a07c2ca7163e1b840b76d1ce08a885
HosseinHosseini <hoseinhoseini2171384@gmail.com>
2021-09-21 01:11:44 +0430

    add team section carousel

--- js/main.js
+++ js/main.js
@@ -110,3 +110,27 @@ $('.testimonials-carousel').owlCarousel({
         },
     },
 })
+
+// ----------------------------------------------------------------
+// --------------------- Team Carousel -----------------
+// ----------------------------------------------------------------
+$('.team-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+
+        600: {
+            items: 2,
+        },
+
+        1000: {
+            items: 3,
+        },
+    },
+})
